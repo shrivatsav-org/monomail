530082225fd3f0f58c2e20a42bd0f0d786ce8df8
shrivatsav-0 <shrivatsav0412@gmail.com>
2026-06-22 18:13:39 +0530

    Bump version to 1.4b for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.shrivatsav.monomail"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.3.9"
+        versionCode = 12
+        versionName = "1.4b"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
         buildConfigField("String", "GOOGLE_CLIENT_ID", "\"$googleClientId\"")
