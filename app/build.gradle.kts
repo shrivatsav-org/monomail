8393284e0cc4b777711418309dc07ff6e2d8c290
shrivatsav-0 <shrivatsav0412@gmail.com>
2026-07-07 22:41:29 +0530

    chore: bump version to 1.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.shrivatsav.monomail"
         minSdk = 26
         targetSdk = 35
-        versionCode = 23
-        versionName = "1.7.0"
+        versionCode = 24
+        versionName = "1.7.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
